055b23fc092b916201cbd3b7fa4336558bb0c349
aayushbtw <21987529+aayushbtw@users.noreply.github.com>
2026-01-28 19:26:21 +0530

    fix: enable embedding

--- superset/config.py
+++ superset/config.py
@@ -558,7 +558,7 @@ DEFAULT_FEATURE_FLAGS: dict[str, bool] = {
     "DASHBOARD_VIRTUALIZATION": True,
     # This feature flag is stil in beta and is not recommended for production use.
     "GLOBAL_ASYNC_QUERIES": False,
-    "EMBEDDED_SUPERSET": False,
+    "EMBEDDED_SUPERSET": True,
     # Enables Alerts and reports new implementation
     "ALERT_REPORTS": False,
     "ALERT_REPORT_TABS": False,
@@ -2116,8 +2116,6 @@ GLOBAL_ASYNC_QUERIES_CACHE_BACKEND = {
     "CACHE_REDIS_SSL_CERT_REQS": "required",
     "CACHE_REDIS_SSL_CA_CERTS": None,
 }
-
-
 
 # Embedded config options
 GUEST_ROLE_NAME = "Public"
