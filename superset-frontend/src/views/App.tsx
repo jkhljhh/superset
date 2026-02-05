f9e71a60cee7dc289010234c1d9aa90467f8d077
jkhljhh <105105676+jkhljhh@users.noreply.github.com>
2026-02-05 16:32:36 +0530

    Favicon+Header changes

--- superset-frontend/src/views/App.tsx
+++ superset-frontend/src/views/App.tsx
@@ -70,6 +70,51 @@ const LocationPathnameLogger = () => {
   }, [location.pathname]);
   return <></>;
 };
+const AppShell = () => {
+ const location = useLocation();
+const isExplore = location.pathname.startsWith('/explore') ;
+
+  const isEmbedded =
+    new URLSearchParams(location.search).get('embed') === '1';
+
+  const hideMenu = isExplore;
+  return (
+    <>
+      {!hideMenu && (
+        <Menu
+          data={bootstrapData.common.menu_data}
+          isFrontendRoute={isFrontendRoute}
+        />
+      )}
+
+      <Switch>
+        {routes.map(({ path, Component, props = {}, Fallback = Loading }) => (
+          <Route path={path} key={path}>
+            <Suspense fallback={<Fallback />}>
+              <Layout>
+                <Layout.Content
+                  css={css`
+                    display: flex;
+                    flex-direction: column;
+                  `}
+                >
+                  <ErrorBoundary
+                    css={css`
+                      margin: 16px;
+                    `}
+                  >
+                    <Component user={bootstrapData.user} {...props} />
+                  </ErrorBoundary>
+                </Layout.Content>
+              </Layout>
+            </Suspense>
+          </Route>
+        ))}
+      </Switch>
+    </>
+  );
+};
+
 
 const App = () => (
   <Router basename={applicationRoot()}>
@@ -77,7 +122,7 @@ const App = () => (
     <LocationPathnameLogger />
     <RootContextProviders>
       <ExtensionsStartup />
-      <Menu
+      {/* <Menu
         data={bootstrapData.common.menu_data}
         isFrontendRoute={isFrontendRoute}
       />
@@ -104,7 +149,8 @@ const App = () => (
             </Suspense>
           </Route>
         ))}
-      </Switch>
+      </Switch> */}
+      <AppShell/>
       <ToastContainer />
     </RootContextProviders>
   </Router>
